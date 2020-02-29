be3da5c909ecac3d2470e6960611cf877e1e0f9e
BohdanBurlay1 <bohdanburlay69@gmail.com>
2020-03-01 00:12:47 +0200

    My custom theme

---
+++ src/js/mytheme.script.js
@@ -0,0 +1,12 @@
+
+import 'popper.js';
+import 'bootstrap';
+(function () {
+  'use strict';
+  Drupal.behaviors.helloWorld = {
+    attach: function (context) {
+      console.log('Hello World');
+    }
+  };
+})(jQuery, Drupal);
+
